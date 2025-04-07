04b1f2348d395b0f09032a499e38c84a54b99723
marya <maryamineirinha@gmail.com>
2025-04-07 19:37:50 -0300

    second commit.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import data
 import helpers
 
@@ -6,38 +7,47 @@ class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
-           print("Conectado ao servidor Urban Routes")
+            print("Conectado ao servidor Urban Routes")
         else:
-           print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+            print("Não é possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução")
 
     def test_set_route(self):
-        print("A função para definir" )
-pass
-
-
-def test_select_plan (self):
-   print( "A função  para definir" )
-pass
-
-def test_fill_phone_number(self):
-    print("A  função para definir" )
-pass
-
-def test_fill_card (self):
-    print("A funcao para definir")
-    pass
-def test_comment_for_driver(self):
-    print("A funcao para definir")
-    pass
-def test_order_blanket_and_handkerchiefs(self):
-    print("A funcao para definir")
-   #Adicionar em S8
-    pass
-def test_order_2_ice_creams(self):
-    number_of_ice_creams = 2
-    for count in range(number_of_ice_creams):
+        print("Para criar a rota")
+        #Adicionar em  S8
         pass
 
-def test_car_search_model_appears(self):
-    print("A funcao para definir")
-    pass
+    def test_select_plan(self):
+        print("Para selecionar um plano")
+        #Adicionar em S8
+        pass
+
+    def test_fill_phone_number(self):
+        print("Adicionar o numero de telefone")
+        #Adicionar em  S8
+        pass
+
+    def test_fill_card(self):
+        print("Adiocnar cartao")
+        #Adicionar em  S8
+        pass
+
+    def test_comment_for_driver(self):
+        print("Adicionar o drive")
+        #Adicionar em  S8
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        print("Ordenar ")
+        #Adicionar em  S8
+        pass
+
+    def test_order_2_ice_creams(self):
+        number_of_ice_creams = 2
+        for count in range(number_of_ice_creams):
+            #Adicionar em  S8
+            pass
+
+    def test_car_search_model_appears(self):
+        print(" Cartao de pesquisa")
+        #Adicionar em  S8
+        pass
